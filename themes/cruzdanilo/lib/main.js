661d7b90a24f4bb796c90727e25f2d012c239d4f
Danilo Neves Cruz <cruzdanilo@gmail.com>
2018-02-24 12:46:48 -0300

    remove browser bar bs

--- themes/cruzdanilo/lib/main.js
+++ themes/cruzdanilo/lib/main.js
@@ -4,7 +4,6 @@ import pressStart2p from './assets/press-start-2p.bdf';
 import Scene from './scene';
 
 function updateSize() {
-  window.scrollTo(0, 1);
   const size = cc.view.getFrameSize();
   cc.view.setDesignResolutionSize(size.width, size.height, cc.ResolutionPolicy.EXACT_FIT);
 }
