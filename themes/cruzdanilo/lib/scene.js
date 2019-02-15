092ed414bfa6322ec424f499b5c4669d7c12bb9d
danilo neves cruz <cruzdanilo@gmail.com>
2019-02-15 19:20:14 -0200

    add tilemap

--- themes/cruzdanilo/lib/scene.js
+++ themes/cruzdanilo/lib/scene.js
@@ -1,5 +1,7 @@
 /* global articles */
 import 'phaser';
+import tilemap from './assets/tilemap';
+import tileset from './assets/tileset.png';
 import { preloadArticles, createArticle, cleanupArticles } from './article';
 
 export default class Scene extends Phaser.Scene {
@@ -15,11 +17,17 @@ export default class Scene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('tileset', tileset);
     preloadArticles(this, articles.map(a => a.querySelector('img').src));
   }
 
   create() {
-    articles.forEach((a, i) => createArticle(this, a, i).setPosition(i * 48 * 3, 0));
+    this.cache.tilemap.add('tilemap', { format: Phaser.Tilemaps.Formats.TILED_JSON, data: tilemap });
+    const map = this.add.tilemap('tilemap');
+    map.addTilesetImage('tileset', 'tileset');
+    this.map = map.createStaticLayer('main', 'tileset').setScale(3);
+    this.articles = articles.map((a, i) => createArticle(this, a, i)
+      .setPosition((48 + (i % 4) * (48 + 16)) * 3, Math.floor(i / 4) * (64 + 16) * 3));
     const camera = this.cameras.main;
     const cursors = this.input.keyboard.createCursorKeys();
     this.controls = new Phaser.Cameras.Controls.FixedKeyControl({
@@ -30,7 +38,7 @@ export default class Scene extends Phaser.Scene {
       down: cursors.down,
       speed: 0.2,
     });
-    camera.setBounds(0, 0, articles.length * 48 * 3, 64 * 3);
+    camera.setBounds(0, 0, this.map.displayWidth, this.map.displayHeight);
   }
 
   update(time, delta) {
