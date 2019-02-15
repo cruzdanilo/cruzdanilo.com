fa7ab8e4cd25b413d3b2a07490d1477ae8719975
danilo neves cruz <cruzdanilo@gmail.com>
2019-02-15 18:19:30 -0200

    open window on hover

--- themes/cruzdanilo/lib/article.js
+++ themes/cruzdanilo/lib/article.js
@@ -7,21 +7,34 @@ export function preloadArticles(scene, images) {
 }
 
 export function createArticle(scene, node, i) {
-  if (!scene.anims.exists('article-frame')) {
+  if (!scene.anims.exists('article-frame-idle')) {
     scene.anims.create({
-      key: 'article-frame',
+      key: 'article-frame-idle',
       frames: scene.anims.generateFrameNumbers('article-frame', { start: 0, end: 1 }),
       frameRate: 1,
       yoyo: true,
       repeat: -1,
     });
+    scene.anims.create({
+      key: 'article-frame-open',
+      frames: scene.anims.generateFrameNumbers('article-frame', { start: 0, end: 2 }),
+      frameRate: 10,
+    });
   }
-  return scene.add.container().add([
-    scene.add.image(11 * 3, 11 * 3, `article-${i}`).setOrigin(0),
-    scene.add.sprite(0, 0, 'article-frame').setOrigin(0).setScale(3).play('article-frame'),
-  ]);
+  const image = scene.add.image(11 * 3, 11 * 3, `article-${i}`).setOrigin(0);
+  const sprite = scene.add.sprite(0, 0, 'article-frame').setOrigin(0).setScale(3)
+    .play('article-frame-idle')
+    .setInteractive()
+    .on('pointerdown', () => {})
+    .on('pointerover', () => sprite
+      .play('article-frame-open', false, sprite.anims.currentFrame.index))
+    .on('pointerout', () => sprite
+      .anims.playReverse('article-frame-open', false, sprite.anims.currentFrame.index)
+      .anims.chain('article-frame-idle'));
+  return scene.add.container().add([image, sprite]);
 }
 
 export function cleanupArticles(scene) {
-  scene.anims.remove('article-frame');
+  scene.anims.remove('article-frame-idle');
+  scene.anims.remove('article-frame-open');
 }
