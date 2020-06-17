a0e42859dc558476c1f8aab01d1d463c3d7d987c
danilo neves cruz <cruzdanilo@gmail.com>
2020-06-18 05:19:54 +0700

    serviceworker: index fallback

--- themes/cruzdanilo/lib/serviceWorker.js
+++ themes/cruzdanilo/lib/serviceWorker.js
@@ -1,9 +1,11 @@
 import { clientsClaim, skipWaiting } from 'workbox-core';
-import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
 import { initialize } from 'workbox-google-analytics';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 
 skipWaiting();
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST); // eslint-disable-line no-underscore-dangle
 cleanupOutdatedCaches();
+registerRoute(new NavigationRoute(createHandlerBoundToURL('/index.html')));
 initialize();
